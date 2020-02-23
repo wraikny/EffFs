9faf59ac9c226e05e960fcabf3604d22fec1f768
wraikny <writtenknown@gmail.com>
2020-02-24 00:29:49 +0900

    add some functions

--- example/example.fsx
+++ example/example.fsx
@@ -4,9 +4,17 @@ open EffFs
 type RandomInt = RandomInt of int
 type Println = Println of obj
 
-let inline foo() =
+let inline hoge() = eff {
+  let! a = RandomInt 10000
+  do! Println "Hello!"
+  return (float a / 10000.0)
+}
+
+let inline foo(): Eff<'a, ^h> =
   eff {
     let! a = RandomInt 100
+    let! x = hoge()
+    do! Println x
     do! Println a
     let b = a + a
     return (a, b)
@@ -18,26 +26,40 @@ module Handlers =
   type Handler1 = Handler1 with
     static member inline Handle(x) = x
 
+    // allow nested effect
+    static member inline Handle(e, k) = Eff.nest e k
+
     static member inline Handle(RandomInt a, k) =
       rand.Next(a) |> k
-    
+
     static member inline Handle(Println a, k) =
       printfn "%A" a; k()
 
-  type Handler2 = Handler2 with
-    static member inline Handle(x) =
-      let a, b = x
-      (a * 1000) + b
+  type Handler2 = {name : string } with
 
-    static member inline Handle(_:Eff<unit,_>, k) = k()
+    static member inline Handle(x) = x
 
     static member inline Handle(RandomInt a, k) =
-      printfn "random: %d" a
-      rand.Next(a) |> k
-    
+      // capture the handler
+      Eff.capture(fun h ->
+        printfn "[%s]: RandomInt(%d)" h.name a
+        rand.Next(a) |> k
+      )
+
     static member inline Handle(Println a, k) =
-      printfn "print: %A" a
-      printfn "%A" a; k()
+      // capture the handler
+      Eff.capture(fun h ->
+        printfn "[%s]: Println(%A)" h.name a
+        printfn "%A" a; k()
+      )
+
+    static member inline Handle(e, k) =
+      // Hack in nested effect
+      Eff.capture(fun h ->
+        printfn "[%s]: Nest(%A)" h.name e
+        e |> Eff.handle { name = h.name + "-Nested"} |> k
+      )
+
 
 foo()
 |> Eff.handle Handlers.Handler1
@@ -48,16 +70,24 @@ printfn "---"
 let bar : Eff<_, Handlers.Handler2> = foo()
 
 bar
-|> Eff.handle Handlers.Handler2
+|> Eff.handle { Handlers.Handler2.name = "Handler2" }
 |> printfn "%A"
 
 // example output
 (*
-100
-(66, 132)
+"Hello!"
+0.7097
+88
+(88, 176)
 ---
-random: 100
-print: 100
-100
-(77, 154)
+[Handler2]: RandomInt(100)
+[Handler2]: Nest(Eff <fun:bar@69-2>)
+[Handler2-Nested]: RandomInt(10000)
+[Handler2-Nested]: Println("Hello!")
+"Hello!"
+[Handler2]: Println(0.0669)
+0.0669
+[Handler2]: Println(95)
+95
+(95, 190)
 *)
--- src/EffFs/EffFs.fs
+++ src/EffFs/EffFs.fs
@@ -3,14 +3,22 @@
 type Eff<'a, ^h> = Eff of (^h -> 'a)
 
 module Eff =
-  let inline handle handler (Eff e: Eff<'a, ^Handler>) = e handler
+  let inline handle (handler: ^handler) (Eff e) = e handler
+
+  let inline nest (Eff e: Eff<'a, ^handler>) k: Eff<'b, ^handler> = Eff(fun h -> e h |> k |> handle h)
+
+  let inline capture(f: ^h -> Eff<'a, ^h>) = Eff(fun h -> f h |> handle h)
+
+  let inline pure' (x: 'a): Eff<'b, ^h> = (^h: (static member Handle:_->_)x)
+
+  let inline bind (f: 'a -> Eff<'b, ^handler>) (e: ^``Effect<'a>``): Eff<'b, ^handler> =
+    ((^handler or ^``Effect<'a>``): (static member Handle:_*_->_)e,f)
+
+  let inline map (f: 'a -> 'b) (e: ^``Effect<'a>``): Eff<'b, ^h> = bind (f >> pure') e
 
 type EffBuilder() =
-  member inline __.Return(x: 'a): Eff<'b, ^h> =
-    Eff(fun _ -> (^h: (static member Handle:_->_)x))
-  member inline __.Bind(eff: ^Ea, f: 'a -> Eff<'b, ^h>): Eff<'b, ^h> =
-    ((^Ea or ^h): (static member Handle:_*_->_)eff, f)
-  member inline __.ReturnFrom(x) = x
-  member inline __.Combine(g,f) = g >> f
+  member inline __.Return(x: 'a): Eff<'b, ^h> = Eff.pure' x
+  member inline __.ReturnFrom(e: ^e) = Eff.bind id e
+  member inline __.Bind(e: ^e, f): Eff<'b, ^h> = Eff.bind f e
 
 let eff = EffBuilder()
