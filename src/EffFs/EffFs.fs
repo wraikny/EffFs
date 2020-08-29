203814823ecbe8933690c7115e44824c9cfdb87a
wraikny <writtenknown@gmail.com>
2020-08-30 05:05:13 +0900

    update

--- src/EffFs/EffFs.fs
+++ src/EffFs/EffFs.fs
@@ -1,17 +1,18 @@
 ﻿namespace EffFs
 
+[<RequireQualifiedAccess>]
+type EffectTypeMarker<'a> = EffectTypeMarker of 'a
+
 [<Struct>]
 type Eff<'a, 'h> = Eff of ('h -> 'a)
 
 [<RequireQualifiedAccess>]
 module Eff =
-  [<RequireQualifiedAccess>]
-  type Marker<'a> = Marker of 'a
 
   let inline private apply h (Eff e) = e h
 
   /// <summary>Mark Effect's type</summary>
-  let inline marker<'a> = Marker.Marker (Unchecked.defaultof<'a>)
+  let inline marker<'a> = EffectTypeMarker.EffectTypeMarker (Unchecked.defaultof<'a>)
 
   /// <summary>Access to handler instance</summary>
   let inline capture (f: ^h -> Eff<'a, ^h>) = Eff(fun h -> f h |> apply h)
@@ -20,7 +21,7 @@ module Eff =
   let inline pure'(x: 'a): Eff<'a, ^h> = Eff(fun _ -> x)
 
   let inline bind(f: 'a -> Eff<'b, ^g>) (e: ^``Effect<'a>``): Eff<'b, ^h>
-    when ^``Effect<'a>``: (static member Effect: ^``Effect<'a>`` -> Marker<'a>) =
+    when ^``Effect<'a>``: (static member Effect: ^``Effect<'a>`` -> EffectTypeMarker<'a>) =
     ((^h or ^g or ^``Effect<'a>``): (static member Handle:_*_->_)e,f)
 
   let inline flatten (e: ^``Effect<^Effect<'a>>``): Eff<'a, ^h> = bind id e
