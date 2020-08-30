76f2be2518b5b34af56b7f035f7df734b2ef6081
wraikny <writtenknown@gmail.com>
2020-08-30 17:51:45 +0900

    add RandomEffect

---
+++ example/example.random.fsx
@@ -0,0 +1,27 @@
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+#load "../src/EffFs/EffFs.fs"
+#load "../src/EffFs/Library/Random.fs"
+
+open EffFs
+open EffFs.Library
+
+let inline f() = eff {
+  let! a = Random.int 1 10
+  let a : int = a
+  return! Random.array 10 (Random.int 0 a)
+}
+
+type Handler() =
+  let rand = System.Random()
+
+  member private __.Rand with get() = rand
+
+  static member inline Handle(x) = x
+  static member inline Handle(e, k) =
+    Eff.capture(fun (h: Handler) -> e |> Random.apply h.Rand |> k)
+
+let handler = Handler()
+
+let result: int[] = f() |> Eff.handle handler
+
+result |> printfn "%A"
---
+++ src/EffFs/Library/Random.fs
@@ -0,0 +1,37 @@
+[<RequireQualifiedAccess>]
+module EffFs.Library.Random
+open EffFs
+
+type RandomEffect<'a> = RandomEffect of (System.Random -> 'a)
+with
+  static member Effect(_: RandomEffect<'a>) = Eff.marker<'a>
+
+let inline apply r (RandomEffect f) = f r
+
+let inline private bind f (RandomEffect random) =
+  RandomEffect (fun r -> (f (random r)) |> apply r)
+
+let bool = RandomEffect (fun r -> r.Next() % 2 = 0)
+
+let int min max = RandomEffect (fun r -> r.Next(min, max))
+
+let float = RandomEffect (fun r -> r.NextDouble())
+
+let float32 = RandomEffect (fun r -> r.NextDouble() |> float32)
+
+let array count (RandomEffect random) =
+  RandomEffect (fun r ->
+    let arr = Array.zeroCreate count
+    for i in 0..count-1 do arr.[i] <- random r
+    arr
+  )
+
+let infinit (RandomEffect random) =
+  RandomEffect (fun r -> seq { while true do yield random r })
+
+let shuffleSeq (xs: seq<'a>) =
+  RandomEffect (fun r -> xs |> Seq.sortBy (fun _ -> r.Next()) )
+
+let shuffleArray (xs: 'a[]) =
+  RandomEffect (fun r -> xs |> Array.sortBy (fun _ -> r.Next()) )
+
