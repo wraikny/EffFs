49406158be836daecbd6c567dd88c18119e3adbe
wraikny <writtenknown@gmail.com>
2021-04-09 13:31:55 +0900

    update randomeffect

--- example/example.random.fsx
+++ example/example.random.fsx
@@ -3,25 +3,24 @@ System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 #load "../src/EffFs/Library/Random.fs"
 
 open EffFs
-open EffFs.Library
+open EffFs.Library.Random
 
 let inline f() = eff {
-  let! a = Random.int 1 10
+  let! a = RandomEffect.int 1 10
   let a : int = a
-  return! Random.array 10 (Random.int 0 a)
+  return! RandomEffect.initArray 10 (RandomEffect.int 0 a)
 }
 
 [<Sealed>]
-type Handler() =
-  let rand = System.Random()
-
-  member private __.Rand with get() = rand
+type Handler(seed) =
+  member val private Rand = System.Random(seed) with get
 
   static member inline Handle(x) = x
-  static member inline Handle(Random.RandomEffect f, k) =
+  static member inline Handle(RandomEffect f, k) =
     Eff.capture(fun (h: Handler) -> f h.Rand |> k)
 
-let handler = Handler()
+
+let handler = Handler(0)
 
 let result: int[] = f() |> Eff.handle handler
 
--- src/EffFs/Library/Random.fs
+++ src/EffFs/Library/Random.fs
@@ -1,4 +1,3 @@
-[<RequireQualifiedAccess>]
 module EffFs.Library.Random
 open EffFs
 
@@ -7,37 +6,74 @@ type RandomEffect<'a> = RandomEffect of (System.Random -> 'a)
 with
   static member Effect(_: RandomEffect<'a>) = Eff.marker<'a>
 
-let inline private apply r (RandomEffect f) = f r
+[<RequireQualifiedAccess>]
+module RandomEffect =
+  let inline private apply r (RandomEffect f) = f r
 
-let inline pure' a = RandomEffect (fun _ -> a)
+  let inline pure' a = RandomEffect (fun _ -> a)
 
-let inline bind f (RandomEffect random) =
-  RandomEffect (fun r -> (f (random r)) |> apply r)
+  let inline bind f (RandomEffect random) =
+    RandomEffect (fun r -> (f (random r)) |> apply r)
 
-let inline map f (RandomEffect random) =
-  RandomEffect (fun r -> random r |> f)
+  let inline map f (RandomEffect random) =
+    RandomEffect (fun r -> random r |> f)
 
-let bool = RandomEffect (fun r -> r.Next() % 2 = 0)
+  let bool = RandomEffect (fun r -> r.Next() % 2 = 0)
 
-let int min max = RandomEffect (fun r -> r.Next(min, max))
+  let int min max = RandomEffect (fun r -> r.Next(min, max))
 
-let float = RandomEffect (fun r -> r.NextDouble())
+  let float = RandomEffect (fun r -> r.NextDouble())
 
-let float32 = RandomEffect (fun r -> r.NextDouble() |> float32)
+  let float32 = RandomEffect (fun r -> r.NextDouble() |> float32)
 
-let array count (RandomEffect random) =
-  RandomEffect (fun r ->
-    let arr = Array.zeroCreate count
-    for i in 0..count-1 do arr.[i] <- random r
-    arr
-  )
+  let tuple2 (RandomEffect r1) (RandomEffect r2) =
+    RandomEffect (fun r -> (r1 r, r2 r))
 
-let infinit (RandomEffect random) =
-  RandomEffect (fun r -> seq { while true do yield random r })
+  let tuple3 (RandomEffect r1) (RandomEffect r2) (RandomEffect r3) =
+    RandomEffect (fun r -> (r1 r, r2 r, r3 r))
 
-let shuffleSeq (xs: seq<'a>) =
-  RandomEffect (fun r -> xs |> Seq.sortBy (fun _ -> r.Next()) )
+  let tuple2V (RandomEffect r1) (RandomEffect r2) =
+    RandomEffect (fun r -> struct(r1 r, r2 r))
 
-let shuffleArray (xs: 'a[]) =
-  RandomEffect (fun r -> xs |> Array.sortBy (fun _ -> r.Next()) )
+  let tuple3V (RandomEffect r1) (RandomEffect r2) (RandomEffect r3) =
+    RandomEffect (fun r -> struct(r1 r, r2 r, r3 r))
 
+  let inline until f (RandomEffect random) =
+    RandomEffect (fun r ->
+      let rec loop () =
+        let x = random r
+        if f x then x else loop ()
+      loop ()
+    )
+
+  let initSeq count (RandomEffect random) =
+    RandomEffect (fun r -> Seq.init count (fun _ -> random r))
+
+  let initArray count (RandomEffect random) =
+    RandomEffect (fun r -> Array.init count (fun _ -> random r))
+
+  let infinit (RandomEffect random) =
+    RandomEffect (fun r -> seq { while true do yield random r })
+
+  let shuffleSeq (xs: 'a seq) =
+    RandomEffect (fun r -> Seq.sortBy (fun _ -> r.Next()) xs)
+
+  let shuffleList (xs: 'a list) =
+    RandomEffect (fun r -> List.sortBy (fun _ -> r.Next()) xs)
+
+  let shuffleArray (xs: 'a[]) =
+    RandomEffect (fun r -> Array.sortBy (fun _ -> r.Next()) xs)
+
+  let inline sequence (rs: _ RandomEffect seq) =
+    RandomEffect (fun r ->
+      rs |> Seq.map (apply r)
+    )
+
+  let inline sequenceList (rs: _ RandomEffect list) =
+    RandomEffect (fun r -> List.map (apply r) rs)
+
+  let inline sequenceArray (rs: _ RandomEffect []) =
+    RandomEffect (fun r -> Array.map (apply r) rs)
+
+  let inline sequenceMap (rs: Map<_, _ RandomEffect>) =
+    RandomEffect (fun r -> Map.map (fun _ -> apply r) rs)
